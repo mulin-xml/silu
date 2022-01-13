15e8aaf82814d0041ff95328435dad751ab51468
xumulin <mlxu18@fudan.edu.cn>
2022-01-13 13:49:39 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 // ignore_for_file: avoid_print
 
 import 'dart:convert';
-import 'dart:ffi';
-import 'dart:io';
-// import 'dart:convert';
+// import 'dart:io';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
